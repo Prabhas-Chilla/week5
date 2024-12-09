a9b35caed5d6967adc2b5d9c6fcf9c9ce682f6ae
Prabhas-Chilla <prabhachilla056@gmail.com>
2024-12-09 22:54:45 +0530

    initial

---
+++ Prime.java
@@ -0,0 +1,9 @@
+public class Prime{
+    public static void main(String args[]){
+        for(int i=2;i<100;i++){
+            if(i%2==0){
+                System.out.println("Prime Numbers"+i);
+            }
+        }
+    }
+}
